820479d9accc56bae137a32ac09f876145e9c2ae
dickreuter <dickreuter@yahoo.com>
2016-08-18 00:32:04 +0100

    GUI update

---
+++ gui/GUI_QT_ui_analyser.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'GUI_QT_ui_analyser.ui'
+#
+# Created: Wed Aug 17 23:51:47 2016
+#      by: pyside-uic 0.2.15 running on PySide 1.2.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(756, 636)
+        self.formLayoutWidget = QtGui.QWidget(Form)
+        self.formLayoutWidget.setGeometry(QtCore.QRect(90, 200, 571, 401))
+        self.formLayoutWidget.setObjectName("formLayoutWidget")
+        self.formLayout = QtGui.QFormLayout(self.formLayoutWidget)
+        self.formLayout.setFieldGrowthPolicy(QtGui.QFormLayout.AllNonFixedFieldsGrow)
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.formLayout.setObjectName("formLayout")
+        self.widget = QtGui.QWidget(self.formLayoutWidget)
+        self.widget.setObjectName("widget")
+        self.formLayout.setWidget(1, QtGui.QFormLayout.LabelRole, self.widget)
+        self.button_analyse = QtGui.QPushButton(Form)
+        self.button_analyse.setGeometry(QtCore.QRect(90, 150, 181, 41))
+        self.button_analyse.setObjectName("button_analyse")
+        self.horizontalLayoutWidget_2 = QtGui.QWidget(Form)
+        self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(90, 80, 571, 31))
+        self.horizontalLayoutWidget_2.setObjectName("horizontalLayoutWidget_2")
+        self.horizontalLayout_2 = QtGui.QHBoxLayout(self.horizontalLayoutWidget_2)
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.label = QtGui.QLabel(self.horizontalLayoutWidget_2)
+        self.label.setObjectName("label")
+        self.horizontalLayout_2.addWidget(self.label)
+        self.label_2 = QtGui.QLabel(self.horizontalLayoutWidget_2)
+        self.label_2.setObjectName("label_2")
+        self.horizontalLayout_2.addWidget(self.label_2)
+        self.label_3 = QtGui.QLabel(self.horizontalLayoutWidget_2)
+        self.label_3.setObjectName("label_3")
+        self.horizontalLayout_2.addWidget(self.label_3)
+        self.horizontalLayoutWidget = QtGui.QWidget(Form)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(90, 115, 571, 31))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtGui.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.combobox_analyse = QtGui.QComboBox(self.horizontalLayoutWidget)
+        self.combobox_analyse.setObjectName("combobox_analyse")
+        self.horizontalLayout.addWidget(self.combobox_analyse)
+        self.combobox_item1 = QtGui.QComboBox(self.horizontalLayoutWidget)
+        self.combobox_item1.setObjectName("combobox_item1")
+        self.horizontalLayout.addWidget(self.combobox_item1)
+        self.combobox_item2 = QtGui.QComboBox(self.horizontalLayoutWidget)
+        self.combobox_item2.setObjectName("combobox_item2")
+        self.horizontalLayout.addWidget(self.combobox_item2)
+        self.label_4 = QtGui.QLabel(Form)
+        self.label_4.setGeometry(QtCore.QRect(270, 10, 261, 31))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        font.setWeight(75)
+        font.setBold(True)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        self.lcdNumber = QtGui.QLCDNumber(Form)
+        self.lcdNumber.setGeometry(QtCore.QRect(560, 150, 101, 41))
+        self.lcdNumber.setObjectName("lcdNumber")
+        self.label_5 = QtGui.QLabel(Form)
+        self.label_5.setGeometry(QtCore.QRect(470, 170, 111, 16))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName("label_5")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        self.button_analyse.setText(QtGui.QApplication.translate("Form", "Show Analysis", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("Form", "Strategy", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("Form", "Item1", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_3.setText(QtGui.QApplication.translate("Form", "Item2", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_4.setText(QtGui.QApplication.translate("Form", "Strategy Analyser", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_5.setText(QtGui.QApplication.translate("Form", "Total profit:", None, QtGui.QApplication.UnicodeUTF8))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    Form = QtGui.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
