133568080795eda29642982fc25cd2d1aaf4c07c
dickreuter <dickreuter@yahoo.com>
2016-11-06 22:04:09 +0000

    Added another test All tests are now passed

--- tests/test_reverseTables.py
+++ tests/test_reverseTables.py
@@ -118,3 +118,33 @@ class TestReverseTables(TestCase):
                 sheet_name = preflop_state.get_reverse_sheetname(abs_pos, t)
 
         self.assertEqual('6R1', sheet_name)
+
+
+
+    def test_reversetable_76(self):
+        strategy = 'Snowie3'
+        # preflop
+        t, p, gui_signals, h, logger = init_table('tests/screenshots/76ss.png', strategy=strategy)
+        l = MagicMock()
+        t.totalPotValue = 0.5
+        t.equity = 0.5
+        t.checkButton = False
+        d = Decision(t, h, p, logger, l)
+        t.isHeadsUp = True
+        t.gameStage = "PreFlop"
+        d.__init__(t, h, p, logger, l)
+        d.preflop_override(t, logger, h, p)
+
+        self.assertEqual('6R4', t.preflop_sheet_name)
+
+        preflop_state = CurrentHandPreflopState()
+        bot_preflop_decision = 'Bet'
+        preflop_state.update_values(t, bot_preflop_decision, h)
+
+        # river round 2
+        t, p, gui_signals, h, logger = init_table('tests/screenshots/76s.png', strategy=strategy)
+        for abs_pos in range(5):
+            if t.other_players[abs_pos]['status'] == 1:
+                sheet_name = preflop_state.get_reverse_sheetname(abs_pos, t)
+
+        self.assertEqual('4', sheet_name)
