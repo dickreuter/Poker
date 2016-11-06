Fixed recognition issue for partypoker Threshold for dollar sign recognition is now in coordinates file

table_analysers/table_screen_based.py
@@ -360,12 +360,13 @@ class TableScreenBased(Table):
     def get_other_player_pots(self):
         func_dict = self.coo[inspect.stack()[0][3]][self.tbl]
         self.gui_signals.signal_status.emit("Get player pots")
-        for n, fd in enumerate(func_dict, start=0):
+        for n in range(5):
+            fd=func_dict[n]
             self.gui_signals.signal_progressbar_increase.emit(1)
             pot_area_image = self.crop_image(self.entireScreenPIL, self.tlc[0] - 20 + fd[0], self.tlc[1] + fd[1] - 20,
                                              self.tlc[0] + fd[2] + 20, self.tlc[1] + fd[3] + 20)
             img = cv2.cvtColor(np.array(pot_area_image), cv2.COLOR_BGR2RGB)
-            count, points, bestfit, minvalue = self.find_template_on_screen(self.smallDollarSign1, img, 0.1)
+            count, points, bestfit, minvalue = self.find_template_on_screen(self.smallDollarSign1, img, float(func_dict[5]))
             has_small_dollarsign = count > 0
             if has_small_dollarsign:
                 pil_image = self.crop_image(self.entireScreenPIL, self.tlc[0] + fd[0], self.tlc[1] + fd[1],