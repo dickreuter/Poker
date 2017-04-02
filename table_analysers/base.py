fixed manual preflop improved ocr guessing if bet < call for allin division by 0 error for metrics calculation

table_analysers/base.py
@@ -297,7 +297,9 @@ class Table(object):
         self.gui_signals.signal_label_number_update.emit('gamenumber', str(int(n)))
 
         total_winnings = self.game_logger.get_strategy_return(p.current_strategy, 9999999)
-        winnings_per_bb_100 = total_winnings / p.selected_strategy['bigBlind'] / n * 100
+
+        winnings_per_bb_100 = total_winnings / p.selected_strategy['bigBlind'] / n * 100 if n > 0 else 0
+
         self.logger.info("Total Strategy winnings: %s", total_winnings)
         self.logger.info("Winnings in BB per 100 hands: %s", np.round(winnings_per_bb_100,2))
         self.gui_signals.signal_label_number_update.emit('winnings', str(np.round(winnings_per_bb_100, 2)))

tests/test_decision.py
@@ -61,7 +61,7 @@ class TestDecision(TestCase):
         t.gameStage = "PreFlop"
 
         d.__init__(t, h, p, l)
-        d.preflop_override(t, logger, h, p)
+        d.preflop_table_analyser(t, logger, h, p)
         self.assertEqual(t.first_raiser_utg, 2)
         # self.assertEqual(t.preflop_sheet_name, '42R3')
 
@@ -76,7 +76,7 @@ class TestDecision(TestCase):
         t.gameStage = "PreFlop"
 
         d.__init__(t, h, p, l)
-        d.preflop_override(t, logger, h, p)
+        d.preflop_table_analyser(t, logger, h, p)
         self.assertEqual(t.first_raiser_utg, 4)
         # self.assertEqual(t.preflop_sheet_name, '22R5')
 
@@ -91,7 +91,7 @@ class TestDecision(TestCase):
         t.gameStage = "PreFlop"
 
         d.__init__(t, h, p, l)
-        d.preflop_override(t, logger, h, p)
+        d.preflop_table_analyser(t, logger, h, p)
         self.assertEqual(t.first_raiser_utg, 2)
         # self.assertEqual(t.preflop_sheet_name, '12R3')
 
@@ -106,7 +106,7 @@ class TestDecision(TestCase):
         t.gameStage = "PreFlop"
 
         d.__init__(t, h, p, l)
-        d.preflop_override(t, logger, h, p)
+        d.preflop_table_analyser(t, logger, h, p)
 
         self.assertEqual(t.first_raiser, 2)
         self.assertEqual(t.second_raiser, 4)
@@ -122,7 +122,7 @@ class TestDecision(TestCase):
         t.gameStage = "PreFlop"
 
         d.__init__(t, h, p, l)
-        d.preflop_override(t, logger, h, p)
+        d.preflop_table_analyser(t, logger, h, p)
 
         self.assertEqual(t.first_raiser_utg, 3)
         self.assertEqual(np.isnan(t.second_raiser_utg), True)
@@ -137,7 +137,7 @@ class TestDecision(TestCase):
         t.isHeadsUp = True
         t.gameStage = "PreFlop"
         d.__init__(t, h, p, l)
-        d.preflop_override(t, logger, h, p)
+        d.preflop_table_analyser(t, logger, h, p)
 
         self.assertEqual(t.first_raiser_utg, 4)
         self.assertEqual(np.isnan(t.second_raiser_utg), True)
@@ -152,7 +152,7 @@ class TestDecision(TestCase):
         t.isHeadsUp = True
         t.gameStage = "PreFlop"
         d.__init__(t, h, p, l)
-        d.preflop_override(t, logger, h, p)
+        d.preflop_table_analyser(t, logger, h, p)
 
         self.assertEqual(t.preflop_sheet_name, '6R1C2')
 
@@ -164,7 +164,7 @@ class TestDecision(TestCase):
         t.isHeadsUp = True
         t.gameStage = "PreFlop"
         d.__init__(t, h, p, l)
-        d.preflop_override(t, logger, h, p)
+        d.preflop_table_analyser(t, logger, h, p)
 
         self.assertEqual(t.preflop_sheet_name, '22R6')
 
@@ -176,7 +176,7 @@ class TestDecision(TestCase):
         t.isHeadsUp = True
         t.gameStage = "PreFlop"
         d.__init__(t, h, p, l)
-        d.preflop_override(t, logger, h, p)
+        d.preflop_table_analyser(t, logger, h, p)
 
         self.assertEqual(t.preflop_sheet_name, '2R1')
 
@@ -188,7 +188,7 @@ class TestDecision(TestCase):
         t.isHeadsUp = True
         t.gameStage = "PreFlop"
         d.__init__(t, h, p, l)
-        d.preflop_override(t, logger, h, p)
+        d.preflop_table_analyser(t, logger, h, p)
 
         self.assertEqual(t.preflop_sheet_name, '4R1')
 
@@ -199,7 +199,7 @@ class TestDecision(TestCase):
         t.isHeadsUp = True
         t.gameStage = "PreFlop"
         d.__init__(t, h, p, l)
-        d.preflop_override(t, logger, h, p)
+        d.preflop_table_analyser(t, logger, h, p)
 
         self.assertEqual(t.preflop_sheet_name, '4R3')
 
@@ -211,6 +211,6 @@ class TestDecision(TestCase):
         t.isHeadsUp = True
         t.gameStage = "PreFlop"
         d.__init__(t, h, p, l)
-        d.preflop_override(t, logger, h, p)
+        d.preflop_table_analyser(t, logger, h, p)
 
         self.assertEqual(t.preflop_sheet_name, '12R4R6')