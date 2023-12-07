25962654b81a5ac55b89e5609012f9f6eb0272d2
Nicolas Dickreuter <dickreuter@gmail.com>
2023-12-07 22:45:28 +0000

    additional restapi

--- poker/restapi_local.py
+++ poker/restapi_local.py
@@ -5,14 +5,17 @@ import uvicorn
 from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 
+from poker.tools.helper import COMPUTER_NAME
 from poker.tools.screen_operations import take_screenshot
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-                   "http://deepermind-pokerbot.com"],  # Allow this origin
+    allow_origins=[
+        "http://localhost:5173",
+        "http://deepermind-pokerbot.com",
+    ],  # Allow this origin
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods
     allow_headers=["*"],  # Allow all headers
@@ -20,6 +23,13 @@ app.add_middleware(
 
 
 def local_restapi():
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],  # You might want to be more specific in production.
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     @app.get("/take_screenshot")
     async def get_screenshot_result():
@@ -29,3 +39,8 @@ def local_restapi():
         return Response(content=image_bytes_io.getvalue(), media_type="image/png")
 
     uvicorn.run(app, host="127.0.0.1", port=8005)
+
+
+@app.get("/get_computer_name")
+async def get_computer_name():
+    return {"computer_name": COMPUTER_NAME}
